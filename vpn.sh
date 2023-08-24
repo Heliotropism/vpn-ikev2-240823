1d9d0d28bd0daade28c7543f5d7a50e2afaec3f9
Heliotropism <Heliotropism@users.noreply.github.com>
2023-08-24 19:25:42 +0500

    Update vpn.sh

--- vpn.sh
+++ vpn.sh
@@ -261,9 +261,7 @@ get_setup_url() {
 }
 
 run_setup() {
-  status=0
-  if tmpdir=$(mktemp --tmpdir -d vpn.XXXXX 2>/dev/null); then
-    
+  
       VPN_IPSEC_PSK="$VPN_IPSEC_PSK" VPN_USER="$VPN_USER" \
       VPN_PASSWORD="$VPN_PASSWORD" \
       VPN_PUBLIC_IP="$VPN_PUBLIC_IP" VPN_L2TP_NET="$VPN_L2TP_NET" \
@@ -274,16 +272,7 @@ run_setup() {
       VPN_PROTECT_CONFIG="$VPN_PROTECT_CONFIG" \
       VPN_CLIENT_VALIDITY="$VPN_CLIENT_VALIDITY" \
       VPN_SKIP_IKEV2="$VPN_SKIP_IKEV2" \
-      /bin/bash "/root/vpn/vpn-ikev2-240823/vpn.sh" || status=1
-    else
-      status=1
-      echo "Error: Could not download VPN setup script." >&2
-    fi
-    /bin/rm -f "$tmpdir/vpn.sh"
-    /bin/rmdir "$tmpdir"
-  else
-    exiterr "Could not create temporary directory."
-  fi
+      "/root/vpn/vpn-ikev2-240823/vpn.sh" \
 }
 
 vpnsetup() {
