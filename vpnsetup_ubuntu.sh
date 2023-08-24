612006bc461ef8327ea94d966fa654f9977744ea
Heliotropism <Heliotropism@users.noreply.github.com>
2023-08-24 20:03:22 +0500

    Update vpnsetup_ubuntu.sh

--- vpnsetup_ubuntu.sh
+++ vpnsetup_ubuntu.sh
@@ -592,9 +592,9 @@ update_iptables() {
     $ipi 2 -m conntrack --ctstate INVALID -j DROP
     $ipi 3 -m conntrack --ctstate "$res" -j ACCEPT
     $ipi 4 -p udp -m multiport --dports 11128,18843,23192 -j ACCEPT
-    $ipi 4 -p udp -m multiport --dports 500,4500 -j DROP
     $ipi 5 -p udp --dport 1701 -m policy --dir in --pol ipsec -j ACCEPT
     $ipi 6 -p udp --dport 1701 -j DROP
+    $ipi 7 -p udp -m multiport --dports 500,4500 -j DROP
     $ipf 1 -m conntrack --ctstate INVALID -j DROP
     $ipf 2 -i "$NET_IFACE" -o ppp+ -m conntrack --ctstate "$res" -j ACCEPT
     $ipf 3 -i ppp+ -o "$NET_IFACE" -j ACCEPT
